f16002c963b0c0eda72c351b3f055fc148fabadf
nahid-sebpo <95094891+nahid-sebpo@users.noreply.github.com>
2024-05-23 01:41:31 +0600

    updated code for clicktag2 issue

---
+++ 22-5-24-RL/Richloads/300x250/300x250/manifest.js
@@ -0,0 +1,340 @@
+FT.manifest({
+    "filename": "index.html",
+    "width": 300,
+    "height": 250,
+    "clickTagCount": 5,
+    "hideBrowsers": [
+        "ie8"
+    ],
+    "richLoads": [
+        {
+            "name": "main_rl",
+            "src": "cunard_master_300x250_RL"
+        }
+    ],
+    "instantAds": [
+        {
+            "name": "feedEndpoint",
+            "type": "text",
+            "default": "https://fm.flashtalking.com/feed/1835/specific/M506C,Q529,Q530?shuffle=true"
+        },
+        {
+            "name": "defaultFeedEndpoint",
+            "type": "text",
+            "default": "https://fm.flashtalking.com/feed/1835/specific/M506C,Q529,Q530?shuffle=true"
+        },
+        {
+            "name": "segmentId",
+            "type": "text",
+            "default": ""
+        },
+        {
+            "name": "frameCount",
+            "type": "text",
+            "default": "4"
+        },
+        {
+            "name": "frameDelay",
+            "type": "text",
+            "default": "3.5"
+        },
+        {
+            "name": "text_bkgd_AnimationType",
+            "type": "text",
+            "default": "fade|slide"
+        },
+        {
+            "name": "F1_headline_txt",
+            "type": "text",
+            "default": "Explore with More"
+        },
+        {
+            "name": "F1_headlineTxt_size_hex_xy",
+            "type": "text",
+            "default": "20,#fff,0,218"
+        },
+        {
+            "name": "F1_subHeadline1_txt",
+            "type": "text",
+            "default": "<strong>Alaska Voyages</strong>"
+        },
+        {
+            "name": "F1_subHeadline1Txt_size_hex_xy",
+            "type": "text",
+            "default": "18,#fff,0,42"
+        },
+        {
+            "name": "F1_subHeadline2_txt",
+            "type": "text",
+            "default": "Receive Onboard Credit<br> and more*"
+        },
+        {
+            "name": "F1_subHeadline2Txt_size_hex_xy",
+            "type": "text",
+            "default": "15,#fff,0,68"
+        },
+        {
+            "name": "F2_headline_txt",
+            "type": "text",
+            "default": "Explore Alaska’s frontier"
+        },
+        {
+            "name": "F2_headlineTxt_size_hex_xy",
+            "type": "text",
+            "default": "20,#fff,0,218"
+        },
+        {
+            "name": "F2_subHeadline1_txt",
+            "type": "text",
+            "default": ""
+        },
+        {
+            "name": "F2_subHeadline1Txt_size_hex_xy",
+            "type": "text",
+            "default": "18,#1d1d1b,0,151"
+        },
+        {
+            "name": "F2_subHeadline2_txt",
+            "type": "text",
+            "default": ""
+        },
+        {
+            "name": "F2_subHeadline2Txt_size_hex_xy",
+            "type": "text",
+            "default": "18,#1d1d1b,0,0"
+        },
+        {
+            "name": "F3_headline_txt",
+            "type": "text",
+            "default": "in Cunard<sup>®</sup> style"
+        },
+        {
+            "name": "F3_headlineTxt_size_hex_xy",
+            "type": "text",
+            "default": "20,#1d1d1b,0,218"
+        },
+        {
+            "name": "F3_subHeadline1_txt",
+            "type": "text",
+            "default": ""
+        },
+        {
+            "name": "F3_subHeadline1Txt_size_hex_xy",
+            "type": "text",
+            "default": "18,#1d1d1b,0,0"
+        },
+        {
+            "name": "F3_subHeadline2_txt",
+            "type": "text",
+            "default": ""
+        },
+        {
+            "name": "F3_subHeadline2Txt_size_hex_xy",
+            "type": "text",
+            "default": "18,#1d1d1b,0,0"
+        },
+        {
+            "name": "F4_headline_txt",
+            "type": "text",
+            "default": "Explore with More"
+        },
+        {
+            "name": "F4_headlineTxt_size_hex_xy",
+            "type": "text",
+            "default": "20,#1d1d1b,0,218"
+        },
+        {
+            "name": "F4_subHeadline1_txt",
+            "type": "text",
+            "default": "<strong>Alaska Voyages</strong>"
+        },
+        {
+            "name": "F4_subHeadline1Txt_size_hex_xy",
+            "type": "text",
+            "default": "18,#1d1d1b,0,42"
+        },
+        {
+            "name": "F4_subHeadline2_txt",
+            "type": "text",
+            "default": "Receive Onboard Credit<br> and more*"
+        },
+        {
+            "name": "F4_subHeadline2Txt_size_hex_xy",
+            "type": "text",
+            "default": "18,#1d1d1b,0,68"
+        },
+        {
+            "name": "legalCtaTxt_toggle",
+            "type": "text",
+            "default": "on|off|off|on"
+        },
+        {
+            "name": "legalCta_txt",
+            "type": "text",
+            "default": "*Restrictions apply"
+        },
+        {
+            "name": "legalCtaTxt_size_hex_F1xy_F2xy_F3xy_F4xy",
+            "type": "text",
+            "default": "7|#fff|188,234|188,234|188,234|188,234"
+        },
+        {
+            "name": "legalOverlay_txt",
+            "type": "text",
+            "default": ""
+        },
+        {
+            "name": "legalOverlayTxt_size_hex",
+            "type": "text",
+            "default": "13|#fff"
+        },
+
+
+        {
+            "name": "cta_toggle",
+            "type": "text",
+            "default": "on|on|on|on"
+        },
+        {
+            "name": "cta_txt",
+            "type": "text",
+            "default": "BOOK NOW >"
+        },
+        {
+            "name": "ctaTxt_size_hex_hexHov",
+            "type": "text",
+            "default": "14,#fff,#fff"
+        },
+        {
+            "name": "ctaBtn_hex_hexHov_xy",
+            "type": "text",
+            "default": "transparent,#84754e,188,206"
+        },
+        {
+           "name": "ctaBtnBorder_hex_hexHov",
+            "type": "text",
+            "default": "#fff,transparent"
+        },
+
+
+        {
+            "name": "logo_toggle",
+            "type": "text",
+            "default": "on|on|on|on"
+        },
+        {
+            "name": "logo_img_xy",
+            "type": "text",
+            "default": "0,0"
+        },
+        {
+            "name": "logo2_toggle",
+            "type": "text",
+            "default": "off|off|off|off"
+        },
+        {
+            "name": "logo2_img_xy",
+            "type": "text",
+            "default": "0,0"
+        },
+        {
+            "name": "arrows_txt",
+            "type": "text",
+            "default": "‹|›"
+        },
+        {
+            "name": "arrowsTxt_size_hex_bgHex_bgOpacity",
+            "type": "text",
+            "default": "40,#fff,#000,0.15"
+        },
+
+
+        {
+            "name": "clickTag1_url",
+            "type": "text",
+            "default": ""
+        },
+        {
+            "name": "clickTag2_url",
+            "type": "text",
+            "default": "https://www.cunard.com/en-us/find-a-cruise/"
+        },
+        {
+            "name": "clickTag3_url",
+            "type": "text",
+            "default": ""
+        },
+        {
+            "name": "clickTag4_url",
+            "type": "text",
+            "default": ""
+        },
+        {
+            "name": "clickTag5_url",
+            "type": "text",
+            "default": ""
+        },
+        {
+            "name": "logo_img",
+            "type": "image",
+            "default": "images/cunard_logo_white_300x250.png"
+        },
+        {
+            "name": "logo2_img",
+            "type": "image",
+            "default": "images/blank.png"
+        },
+        {
+            "name": "F1_bkgd_img",
+            "type": "image",
+            "default": "images/Flashtalking-300x250-alaska2.jpg"
+        },
+        {
+            "name": "F2_bkgd_img",
+            "type": "image",
+            "default": "images/Flashtalking-300x250-alaska3.jpg"
+        },
+        {
+            "name": "F3_bkgd_img",
+            "type": "image",
+            "default": "images/Flashtalking-300x250-tea.jpg"
+        },
+        {
+            "name": "F4_bkgd_img",
+            "type": "image",
+            "default": "images/Flashtalking-300x250-alaska2.jpg"
+        },
+        {
+            "name": "failsafe_img",
+            "type": "image",
+            "default": "blank.png"
+        },
+        {
+            "name": "main_rl",
+            "type": "richload",
+            "default": "cunard_master_300x250_RL/index.html"
+        }
+    ],
+    "trackingEvents": [
+        {
+            "name": "left_swipe",
+            "type": "string"
+        },
+        {
+            "name": "right_swipe",
+            "type": "string"
+        },
+        {
+            "name": "legalCta_click",
+            "type": "string"
+        },
+        {
+            "name": "left_arrow_click",
+            "type": "string"
+        },
+        {
+            "name": "right_arrow_click",
+            "type": "string"
+        }
+    ]
+});
